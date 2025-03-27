dd254d1c3584451619b4fc944f3e4ed1f3c29b23
Salman Izzuddin <salmanizzu.dev@protonmail.com>
2025-03-27 16:32:35 +0700

    contract(feat): create contract with addPerson, updateName, updateFavoriteNumber, and getMyFavoriteNumber features

---
+++ src/SmartAddressBook.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SmartAddressBook {
+    error SmartAddressBook__NotOwner();
+
+    uint256 number;
+    address public owner;
+
+    event MemberAdded(address indexed user, string name, uint256 favoriteNumber);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct Member {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    Member[] public memberList;
+
+    mapping(address => Member) public addressToMember;
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert SmartAddressBook__NotOwner();
+        }
+        _;
+    }
+
+    function addPerson(string memory _name, uint256 _favNum) public {
+        Member memory newMember = Member(_name, _favNum);
+        memberList.push(newMember);
+        addressToMember[msg.sender] = newMember;
+        emit MemberAdded(msg.sender, _name, _favNum);
+    }
+
+    function updateName(string memory _name) public onlyOwner {
+        addressToMember[msg.sender].name = _name;
+    }
+
+    function updateFavoriteNumber(uint256 _favNum) public onlyOwner {
+        addressToMember[msg.sender].favoriteNumber = _favNum;
+    }
+
+    function getMyFavoriteNumber() external view returns (uint256) {
+        return addressToMember[msg.sender].favoriteNumber;
+    }
+}
